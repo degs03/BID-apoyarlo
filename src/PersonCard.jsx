1b5968562f5ae3f67a14fab56e43db56f4681e43
Dario Gimenez <gimenezdario1617@gmail.com>
2023-01-13 22:56:54 -0300

    Add age

--- src/PersonCard.jsx
+++ src/PersonCard.jsx
@@ -1,13 +1,22 @@
 import { Component } from "react";
+import React from "react";
 class PersonCard extends Component {
+    constructor(props){
+        super(props)
+
+        this.state = {counter : this.props.age}
+    }
     render(){
-    const {firstName, lastName, age, hairColor} = this.props;
+    const {firstName, lastName, hairColor} = this.props;
     return (
+        <>
         <div className="personCard">
             <h1>{firstName},{lastName}</h1>
-            <p>Age: {age}</p>
-            <p>Hair color: {hairColor}</p>
+            <p>Age: {this.state.counter}</p>
+            <p>Hair color: {hairColor}</p> 
+            <button onClick={()=> this.setState({counter: this.state.counter+1}) }>Birthday Button for {firstName} {lastName}</button>
         </div>
+    </> 
     );
     }
 }
